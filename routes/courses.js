63140ed0ffce184ab18e06a313f3ec7762f0e125
cuong220520 <cuongNDGBS18195@fpt.edu.vn>
2020-03-19 20:43:29 +0700

    finish course REST

---
+++ routes/courses.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const router = express.Router()
+
+const Course = require('../models/course')
+
+router.get('/', async (req, res) => {
+    let courses
+    try {
+        courses = await Course.find().sort('desc')
+    } catch {
+        courses = []
+    }
+    res.render('courses/index', { courses: courses })
+})
+
+router.get('/new', (req, res) => {
+    renderNewPage(res, new Course())
+})
+
+router.post('/', async (req, res) => {
+    const course = new Course({
+        name: req.body.name,
+        code: req.body.code
+    })
+    try {
+        const newCourse = await course.save()
+        res.redirect(`/courses/${newCourse.id}`)
+    } catch {
+        renderNewPage(res, course, true)
+    }
+})
+
+router.get('/:id', async(req, res) => {
+    try {
+        const course = await Course.findById(req.params.id)
+        res.render('courses/show', {course: course})
+    } catch {
+        res.redirect('/courses')
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.id)
+        renderEditPage(res, course)
+    } catch {
+        res.redirect('/courses')
+    }
+})
+
+router.put('/:id', async(req, res) => {
+    let course 
+    try {
+        course = await Course.findById(req.params.id)
+        course.name = req.body.name
+        course.code = req.body.code
+        await course.save()
+        res.redirect(`/courses/${course.id}`)
+    } catch {
+        renderEditPage(res, course, true)
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    let course
+    try {
+        course = await Course.findById(req.params.id)
+        await course.remove()
+        res.redirect('/courses')
+    } catch {
+        res.render('courses/index', {
+            course: course,
+            errorMessage: 'Deleting course error!'
+        })
+    }
+})
+
+function renderNewPage(res, course, hasError = false) {
+    renderFormPage(res, course, 'new', hasError)
+}
+
+function renderEditPage(res, course, hasError = false) {
+    renderFormPage(res, course, 'edit', hasError)
+}
+
+function renderFormPage(res, course, form, hasError = false) {
+    try {
+        const params = {
+            course: course
+        }
+        if (hasError) {
+            if (form == 'edit') {
+                params.errorMessage = 'Updating course error!'
+            } else {
+                params.errorMessage = 'Creating course error!'
+            }
+        }
+        res.render(`courses/${form}`, params)
+    } catch {
+        res.redirect('/courses')
+    }
+}
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,13 +5,20 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
+const assignmentRouter = require('./routes/assignments')
+const courseRouter = require('./routes/courses')
 
+app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(methodOverride('_method'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,6 +27,8 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/assignments', assignmentRouter)
+app.use('/courses', courseRouter)
 
 app.listen(process.env.PORT || 3000)
 
